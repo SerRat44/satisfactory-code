061449e38d87d57e4f6ad6b47f2463d8d6f54cce
SerRat44 <narlytoes@gmail.com>
2024-12-05 23:24:30 -0800

    fix

--- factories/heavy-oil/control.lua
+++ factories/heavy-oil/control.lua
@@ -1,72 +1,74 @@
-local colors = require('common.colors')
-local utils = require('common.utils')
-local config = require('config')
-local Display = require('display')
-local Power = require('power')
-local Monitoring = require('monitoring')
+return function(dependencies)
+    local colors = dependencies.colors
+    local utils = dependencies.utils
+    local config = dependencies.config
+    local Display = dependencies.display
+    local Power = dependencies.power
+    local Monitoring = dependencies.monitoring
 
--- Initialize modules
-local display = Display:new(component.proxy(config.COMPONENT_IDS.DISPLAY_PANEL))
-local modules = display:initialize()
+    -- Initialize modules
+    local display = Display:new(component.proxy(config.COMPONENT_IDS.DISPLAY_PANEL))
+    local modules = display:initialize()
 
-local power = Power:new(modules)
-local monitoring = Monitoring:new(modules)
+    local power = Power:new(modules, dependencies)
+    local monitoring = Monitoring:new(modules, dependencies)
 
--- Initialize components
-power:initialize()
-monitoring:initialize()
+    -- Initialize components
+    power:initialize()
+    monitoring:initialize()
 
--- Network setup
-local net = computer.getPCIDevices(classes.NetworkCard)[1]
-net:open(101)
-event.listen(net)
+    -- Network setup
+    local net = computer.getPCIDevices(classes.NetworkCard)[1]
+    net:open(101)
+    event.listen(net)
 
-local dataCollectionActive = true
+    local dataCollectionActive = true
 
-function handleNetworkMessage(type, data)
-    if type == "collection" then
-        dataCollectionActive = data
-    else
-        power:handleNetworkMessage(type, data)
+    function handleNetworkMessage(type, data)
+        if type == "collection" then
+            dataCollectionActive = data
+        else
+            power:handleNetworkMessage(type, data)
+        end
     end
-end
 
--- Main control loop
-function main()
-    while true do
-        local e, s, sender, port, type, data = event.pull(config.UPDATE_INTERVAL)
+    -- Main control loop
+    function main()
+        while true do
+            local e, s, sender, port, type, data = event.pull(config.UPDATE_INTERVAL)
 
-        if e == "ChangeState" then
-            local powerAction = power.powerControls[s.Hash]
-            if powerAction then
-                powerAction()
-            end
-        elseif e == "Trigger" then
-            if s == modules.factory.emergency_stop then
-                monitoring:handleEmergencyStop()
-            else
-                for i, button in ipairs(modules.factory.buttons) do
-                    if s == button then
-                        monitoring:handleButtonPress(i)
-                        break
+            if e == "ChangeState" then
+                local powerAction = power.powerControls[s.Hash]
+                if powerAction then
+                    powerAction()
+                end
+            elseif e == "Trigger" then
+                if s == modules.factory.emergency_stop then
+                    monitoring:handleEmergencyStop()
+                else
+                    for i, button in ipairs(modules.factory.buttons) do
+                        if s == button then
+                            monitoring:handleButtonPress(i)
+                            break
+                        end
                     end
                 end
+            elseif e == "NetworkMessage" then
+                handleNetworkMessage(type, data)
             end
-        elseif e == "NetworkMessage" then
-            handleNetworkMessage(type, data)
-        end
 
-        -- Update displays
-        monitoring:updateProductivityHistory()
-        power:updatePowerDisplays()
-        power:updatePowerIndicators()
+            -- Update displays
+            monitoring:updateProductivityHistory()
+            power:updatePowerDisplays()
+            power:updatePowerIndicators()
 
-        -- Broadcast status
-        if dataCollectionActive then
-            monitoring:broadcastRefineryStatus()
-            power:broadcastPowerStatus()
+            -- Broadcast status
+            if dataCollectionActive then
+                monitoring:broadcastRefineryStatus()
+                power:broadcastPowerStatus()
+            end
         end
     end
-end
 
-main()
+    main()
+end
