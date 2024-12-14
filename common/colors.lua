31ddcbfd5ce069ae7b1ad633e4ac554aaf274156
SerRat44 <narlytoes@gmail.com>
2024-12-14 09:12:35 -0800

    fix

--- common/colors.lua
+++ common/colors.lua
@@ -6,7 +6,7 @@ return {
         GREEN = { 0, 255, 0 },
         BLUE = { 0, 0, 255 },
         YELLOW = { 255, 255, 0 },
-        ORANGE = { 240, 40, 0 },
+        ORANGE = { 230, 44, 0 },
     },
     EMIT = {
         INDICATOR = 5,
